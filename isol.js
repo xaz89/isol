855aed504bba5744e32e63861a02fa0ec92de02d
xaz89 <dcg.jip@hotmail.com>
2020-03-11 17:47:51 -0600

    mas animacion

--- isol.js
+++ isol.js
@@ -1,71 +1,71 @@
-gsap.to(".inicio", {
-    duration: 1,
-    opacity: 1,
-    scale: 0.95,
+ gsap.to(".inicio", {
+     duration: 1,
+     opacity: 1,
+     scale: 0.95,
 
-}, 1.5);
-gsap.to(".isol", {
-    duration: 1,
-    opacity: 0.5,
-    scale: 200,
-
-});
-var tl = gsap.timeline();
-//sequenced one-after-the-other
-tl.to("body", {
-    duration: 1,
-    backgroundColor: "#fff",
-    borderRadius: "20px",
-    border: "0px solid white",
-    ease: "ease-in",
-    color: "#fff",
-}) //notice that there's no semicolon!
-    .to(".isol", {
-        duration: 1,
-        opacity: 1,
-        scale: 1,
-        // rotation: 160 * Math.PI / 180
-    }, 0.5)
-    .fromTo(".izquierda p", {
-        duration: 0,
-        x: "-1500%",
-
-    },
-        {
-            stagger: {
-                each: .05,
-                from: "random"
-            },
-            x: 0,
-        }, 1)
-    .fromTo(".derecha p", {
-        duration: 0,
-        x: "1500%",
-    },
-        {
-            stagger: 0.05,
-            x: 0,
-        }, 1)
-//   .to(".contacto", {duration: 3, rotation: 360});
-
-
-
-
-
-/* gsap.to("body", {
-    duration: 2,
-    backgroundColor: "#fff",
-    borderRadius: "20px",
-    border: "0px solid white",
-    ease: "ease-in",
-    color: "#fff",
-    fill: "blue",
-});
+ }, 1.5);
  gsap.to(".isol", {
-    duration: 1,
-    opacity: 0.5,
-    scale: 2.5
-});  */
+     duration: 1,
+     opacity: 0.5,
+     scale: 200,
+
+ });
+ var tl = gsap.timeline();
+ //sequenced one-after-the-other
+ tl.to("body", {
+     duration: 1,
+     backgroundColor: "#fff",
+     // borderRadius: "20px",
+     border: "0px solid white",
+     ease: "ease-in",
+     color: "#fff",
+ }) //notice that there's no semicolon!
+     .to(".isol", {
+         duration: 1,
+         opacity: 1,
+         scale: 1,
+         // rotation: 160 * Math.PI / 180
+     }, 0.5)
+     .fromTo(".izquierda p", {
+         duration: 0,
+         x: "-1500%",
+
+     },
+         {
+             stagger: {
+                 each: .05,
+                 from: "random"
+             },
+             x: 0,
+         }, 1)
+     .fromTo(".derecha p", {
+         duration: 0,
+         x: "1500%",
+     },
+         {
+             stagger: 0.05,
+             x: 0,
+         }, 1)
+ //   .to(".contacto", {duration: 3, rotation: 360});
+
+
+
+
+
+ /* gsap.to("body", {
+     duration: 2,
+     backgroundColor: "#fff",
+     borderRadius: "20px",
+     border: "0px solid white",
+     ease: "ease-in",
+     color: "#fff",
+     fill: "blue",
+ });
+  gsap.to(".isol", {
+     duration: 1,
+     opacity: 0.5,
+     scale: 2.5
+ });  */
 function book() {
     gsap.to("body", {
         backgroundColor: "#f1f1f1",
@@ -78,7 +78,7 @@ function book() {
             duration: 1.5,
             width: 0,
             ease: "power4",
-            x: "-95%",
+            x: "-100%",
         });
     gsap.fromTo(".contacto", {
         width: 0,
@@ -88,29 +88,93 @@ function book() {
             duration: 1.5,
             width: "100%",
             ease: "power4",
-            x: "95%",
-        });
-};
+            x: "100%",
+    });
+    gsap.to(".portafolio", {
+        y:"-100%",
+    }, )
+}; 
 
-gsap.fromTo(".contacto", {
-    width: 0,
-    x: 300,
-},
-    {
-        duration: 1.5,
-        width: "100%",
-        ease: "power3.out",
-        x: 0,
+
+
+function redes() {
+    gsap.to("body", {
+        backgroundColor: "#f1f1f1",
     });
-gsap.fromTo(".book", {
-    width: 0,
-    x: -300,
-},
-    {
-        duration: 1.5,
+    gsap.fromTo(".book", {
         width: "100%",
-        ease: "power3.out",
         x: 0,
+    },
+        {
+            duration: 1.5,
+            width: 0,
+            ease: "power4",
+            x: "-100%",
+        });
+    gsap.fromTo(".contacto", {
+        width: 0,
+        x: 0,
+    },
+        {
+            duration: 1.5,
+            width: "100%",
+            ease: "power4",
+            x: "100%",
     });
-// gsap.to("#isol-trazo", { duration: 1, morphSVG: "#isol-trazo2" });
-// gsap.to(".isol", {duration: 1, rotation: 160 * Math.PI / 180});
+    gsap.to(".portafolio", {
+        y:"100%",
+    }, );
+    gsap.to(".redes", {
+        y:"-200%",
+    }, )
+}; 
+
+
+
+ gsap.fromTo(".contacto", {
+     width: 0,
+     x: 300,
+ },
+     {
+         duration: 1.5,
+         width: "100%",
+         ease: "power3.out",
+         x: 0,
+     });
+ gsap.fromTo(".book", {
+     width: 0,
+     x: -300,
+ },
+     {
+         duration: 1.5,
+         width: "100%",
+         ease: "power3.out",
+         x: 0,
+     });
+ // gsap.to("#isol-trazo", { duration: 1, morphSVG: "#isol-trazo2" });
+ // gsap.to(".isol", {duration: 1, rotation: 160 * Math.PI / 180});
+
+
+/* 
+var tl = gsap.timeline();
+
+ tl.fromTo(".inicio", {
+     opacity: 0,
+     scale: 1,
+
+ },
+ {
+    duration: 1,
+    opacity: 1,
+    scale: 0.95,
+ }, 0);
+ tl.fromTo(".isol", {
+     opacity: 1,
+     scale: 100,
+
+    },
+    {
+       duration: 0.5,
+       opacity: 1,
+       scale: 0.95,
+    }, 1.5); */
--- main.js
+++ main.js
@@ -1,32 +1,69 @@
+var controller = new ScrollMagic.Controller();
 
+
+var scene = new ScrollMagic.Scene({
+  triggerElement: '.contacto',
+  duration: "100%",
+  triggerHook: 0.25,
+  reverse: true,
+})
+.setClassToggle('.contacto', 'entra')
+.addIndicators({
+  name: 'fade scene',
+  colorTrigger: 'gray',
+  colorStart: 'blue',
+  colorEnd: 'blue',
+  // indent: 20
+})
+.addTo(controller);
+
+
+
+
+
+
+
+
+
+
+
+/* 
 var controller = new ScrollMagic.Controller();
 var tl = new TimelineMax();
 tl.fromTo(
-  "section.panel.turqoise",
-  1,
-  { xPercent: 100 },
-  { xPercent: 0, ease: Linear.easeNone },
+  ".inicio",
+  20,
+  { xPercent: 0 },
+  { xPercent: 100, ease: Linear.easeNone },
   "+=1"
 );
 tl.fromTo(
-  "section.panel.bordeaux",
-  1,
-  { yPercent: 100 },
+  ".book",
+  4,
+  { yPercent: -1000 },
   { yPercent: 0, ease: Linear.easeNone },
-  "+=1"
+  "+=14"
+);
+tl.fromTo(
+  ".contacto",
+  4,
+  { yPercent: 1000 },
+  { yPercent: 0, ease: Linear.easeNone },
+  "+=14"
 );
 
 new ScrollMagic.Scene({
-  triggerElement: "#pinMaster",
+  triggerElement: ".book-",
   triggerHook: "onLeave",
   duration: "100%"
 })
-  .setPin("#pinMaster")
+  .setPin(".isol-")
   .setTween(tl)
   .addIndicators({
-    colorTrigger: "white",
-    colorStart: "white",
-    colorEnd: "white",
+    colorTrigger: "blue",
+    colorStart: "blue",
+    colorEnd: "blue",
     indent: 40
   })
   .addTo(controller);
+ */
